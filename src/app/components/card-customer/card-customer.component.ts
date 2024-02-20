d516d70d69ddda59025afb2bc233e1a8d00dc039
freiremat <urbsfreire@gmail.com>
2024-02-20 11:41:04 -0300

    - add card customer comp - add styles + texts

---
+++ src/app/components/card-customer/card-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-customer',
+  templateUrl: './card-customer.component.html',
+  styleUrls: ['./card-customer.component.scss']
+})
+export class CardCustomerComponent {
+
+}
